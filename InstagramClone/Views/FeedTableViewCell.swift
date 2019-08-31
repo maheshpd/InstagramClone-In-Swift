8dae8a696cf7e749135e36abf00f8f50d456b243
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-08-31 14:27:09 +0530

    FeedTableViewCell and StoryTableViewCell Code

--- InstagramClone/Views/FeedTableViewCell.swift
+++ InstagramClone/Views/FeedTableViewCell.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userProfileName: UIButton!
+    
+    @IBOutlet weak var postImage: UIImageView!
+    
+    @IBOutlet weak var likesCountLbl: UILabel!
+    @IBOutlet weak var postCommentLbl: UILabel!
+    @IBOutlet weak var commentCountButton: UIButton!
+    
+    @IBOutlet weak var dateLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        profileImage.layer.cornerRadius = profileImage.frame.width / 2
+        profileImage.layer.masksToBounds = true
+        selectionStyle = UITableViewCell.SelectionStyle.none
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
