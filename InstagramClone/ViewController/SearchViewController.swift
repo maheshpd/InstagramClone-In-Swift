4bc062b3ae448f750a50604b580a1015a256dbd8
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-08-31 17:02:48 +0530

    ExploreCollectionViewCell

--- InstagramClone/ViewController/SearchViewController.swift
+++ InstagramClone/ViewController/SearchViewController.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var searchCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        searchCollectionView.delegate = self
+        searchCollectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        
+        return cell
+    }
+    
 }
---
+++ InstagramClone/Views/ExploreCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreCollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by Mahesh Prasad on 31/08/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var exploreImage: UIImageView!
+    
+    
+    
+}
