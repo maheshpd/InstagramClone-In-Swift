8047e2631939be9b796a13d269e7d3f557bd5406
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-08-31 18:11:02 +0530

    Creating the search

--- InstagramClone/ViewController/SearchViewController.swift
+++ InstagramClone/ViewController/SearchViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var searchCollectionView: UICollectionView!
     
+    var searchController: UISearchController!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +19,26 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UICollec
         
         searchCollectionView.delegate = self
         searchCollectionView.dataSource = self
+        searchCollection = UISearchController(searchResultsController: nil)
+        searchController.obscuresBackgroundDuringPresentation = true
+        searchController.searchBar.showsCancelButton = false
+        
+        for subView in searchController.searchBar.subviews {
+            for subView1 in subView.subviews {
+                if let textField = subView1 as? UITextField {
+                    textField.backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1.0)
+                    
+                    textField.textAlignment = NSTextAlignment.center
+                }
+            }
+        }
+        
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        let searchBarController = SearchBarContainerView(customeSearchBar: searchController.searchBar)
+        searchBarController.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 44)
+        navigationItem.titleView = searchBarController
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
---
+++ InstagramClone/Views/SearchBarContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  SearchBarContainerView.swift
+//  InstagramClone
+//
+//  Created by Mahesh Prasad on 31/08/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarContainerView: UIView {
+
+    let searchBar: UISearchBar
+    
+    init(customeSearchBar: UISearchBar) {
+        searchBar = customeSearchBar
+        super.init(frame: CGRect.zero)
+        
+        addSubview(searchBar)
+    }
+    
+    convenience  override init(frame: CGRect) {
+        self.init(customeSearchBar: UISearchBar())
+        self.frame = frame
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
